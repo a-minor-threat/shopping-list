af24bdf31c1b015bf405578d357c0ed92d28b211
a-minor-threat <dennis.k.shah@gmail.com>
2015-09-30 16:18:57 -0400

    added some new functions

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -2,6 +2,7 @@
 		console.log("jQuery ready!")
 		deleteAction();
 		doneAction();
+		inputValue();
 	});
 
 	function deleteAction () {
@@ -16,4 +17,16 @@
 			console.log("done event fired.")
 			alert("Handler for the done button called.");
 		});		
+	}
+
+	function inputValue() {
+		$(".input-form").keypress(function(event){
+			console.log("keys are being pressed");
+			var value = $(".input-form").val();
+			if (event.which === 13) {
+				console.log("enter key has been pressed");
+				$(".item-name").text(value);
+			}
+
+		});
 	}
