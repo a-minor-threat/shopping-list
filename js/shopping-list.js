2a989f905c84e78234511e44329485a2c8b12cb2
a-minor-threat <dennis.k.shah@gmail.com>
2015-09-30 11:23:47 -0400

    WIP

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -1,11 +1,19 @@
 	$(document).ready(function(){
 		console.log("jQuery ready!")
-		$(".done").click(function(){
-			console.log("done event fired.")
-			alert("Handler for the done button called.");
-		});
+		deleteAction();
+		doneAction();
+	});
+
+	function deleteAction () {
 		$(".delete").click(function(){
 			console.log("delete event fired.")
 			alert("Handler for the delete button called.");
-		});
-	});
+		});		
+	}
+
+	function doneAction() {
+		$(".done").click(function(){
+			console.log("done event fired.")
+			alert("Handler for the done button called.");
+		});		
+	}
