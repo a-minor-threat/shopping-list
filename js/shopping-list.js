f61216c29611eff62eb19b285e823f1e6a3b7ebf
a-minor-threat <dennis.k.shah@gmail.com>
2015-10-01 14:01:51 -0400

    WIP

--- js/shopping-list.js
+++ js/shopping-list.js
@@ -1,40 +1,40 @@
-	$(document).ready(function(){
+	$(document).ready(function() {
 		console.log("jQuery ready!");
 		$(".buttons").hide();
 		$(".item").hide();
-		inputValue();		
+		inputValue();
 		deleteAction();
 		doneAction();
 
 	});
 
 	function inputValue() {
-		$(".input-form").keypress(function(event){
+		$(".input-form").keypress(function(event) {
 			console.log("keys are being pressed");
 			var value = $(".input-form").val();
-		
+
 			if (event.which === 13) {
 				console.log("enter key has been pressed");
 				$(".buttons").fadeIn("slow");
-				$(".item").fadeIn("slow");					
+				$(".item").fadeIn("slow");
 				$(".item-name").text(value);
 				$(".input-form").val('');
 			}
 		});
 	}
 
-	function deleteAction () {
-		$(".delete").click(function(){
+	function deleteAction() {
+		$(".delete").click(function() {
 			console.log("delete event fired.")
 			$(".item-name").remove();
 			$(".buttons").fadeOut("slow");
 			$(".item").fadeOut("slow");
-		});		
+		});
 	}
 
 	function doneAction() {
-		$(".done").click(function(){
+		$(".done").click(function() {
 			console.log("done event fired.")
 			$(".item-name").toggleClass("strike");
-		});		
+		});
 	}
